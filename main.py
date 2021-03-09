5214d2f8f01e03c47421b58fd9e09b305691d52e
Rafael Dantas <rafagd@gmail.com>
2021-03-09 11:09:21 +0000

    Comments and some new tests

--- main.py
+++ main.py
@@ -5,20 +5,28 @@ import json
 import coords    as cd
 import customers as cs
 
+# Class representing the application
 class App:
     office    = None
     customers = None
     invited   = None
 
+    # Construction initializes the terms
     def __init__(self):
         self.office    = cd.Coords(53.339428, -6.257664)
         self.customers = []
         self.invited   = []
 
+    # Reads the customer from a file, provided by caller
     def read_customers(self, fname):
+        # Clears previous configuration
         self.customers.clear()
+
+        # Closes the file when f gets out of scope
         with open(fname, 'r') as f:
             for line in f:
+                # I'm parsing the json here and converting the data to the types
+                # expected by the internal apis.
                 jsline = json.loads(line)
                 coords = cd.Coords(
                     float(jsline['latitude']),
@@ -28,16 +36,26 @@ class App:
                     cs.Customer(jsline['user_id'], jsline['name'], coords)
                 )
 
+    # Invites all customers inside a circle radius (projected on Earth)
     def invite_near_customers(self, distance):
+        # Clears previous configuration
         self.invited.clear()
+
         for customer in self.customers:
+            # Check distance between office and customer
             dst = customer.coords.distance(self.office)
+            # Invites if less than argument
             if dst <= distance:
                 self.invited.append(customer)
+
+        # The list is sorted in ascending order by user_id
         self.invited.sort(key = lambda c: c.user_id)
 
+    # Write the invited array to file
     def write_invited(self, fname):
         with open(fname, 'w+') as f:
+            # Just go through all the invited customers in the array and 
+            # print their id and name separated by a single tab to file
             for customer in self.invited:
                 f.write('{}\t{}\n'.format(customer.user_id, customer.name))
 
@@ -48,10 +66,11 @@ if __name__ == '__main__':
 
     # Collect the arguments of the program, if they have been passed.
     # Otherwise, assigns default values
-    dst      = sys.argv[1] if len(sys.argv) > 1 else 100
+    dst      = int(sys.argv[1]) if len(sys.argv) > 1 else 100
     inp_file = sys.argv[2] if len(sys.argv) > 2 else 'customers.txt'
     out_file = sys.argv[3] if len(sys.argv) > 3 else 'output.txt'
 
+    # Application execution
     app = App()
     app.read_customers(inp_file)
     app.invite_near_customers(dst)
--- test_application.py
+++ test_application.py
@@ -2,6 +2,7 @@ import coords
 import customers
 import math
 import random
+import main
 
 # Test if coods are being created and if their
 # lats/lons are in radians after creation
@@ -32,6 +33,7 @@ def test_distance():
     assert int(c1.distance(c3)) == 20015
     assert int(c3.distance(c1)) == 20015
 
+# Tests if the customer creation works as intended
 def test_customer():
     auid  = int(random.uniform(0, 1000))
     alat  = random.uniform(-90, 90)
@@ -43,4 +45,38 @@ def test_customer():
     assert alice.coords.latitude  == math.radians(alat)
     assert alice.coords.longitude == math.radians(along)
 
+# Testing the creation of the application
+def test_application():
+    app = main.App()
+
+    assert app.office.latitude  == math.radians(53.339428)
+    assert app.office.longitude == math.radians(-6.257664)
+    
+# Test if the read_customers command is reading the whole file    
+def test_read_customers():
+    app = main.App()
+    app.read_customers('customers.txt')
+
+    assert len(app.customers) == 32
+
+# Test inviting customers in two different distances
+def test_invite_near_customers():
+    app = main.App()
+    app.read_customers('customers.txt')
+
+    app.invite_near_customers(100)
+    assert len(app.invited) == 16
+
+    app.invite_near_customers(50)
+    assert len(app.invited) == 8
+
+# Testing the output production
+def test_write_invited():
+    app = main.App()
+    app.read_customers('customers.txt')
+    app.invite_near_customers(100)
+    app.write_invited('output.txt')
+    
+    with open('output.txt', 'r+') as f:
+        assert len(f.readlines()) == 16
 
